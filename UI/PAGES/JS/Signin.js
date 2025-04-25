576c1fe875cf7dddcc03048fb78fdc795af8a89f
Jest <jeremiah.lirag@gmail.com>
2025-04-25 10:46:17 +0800

    UPDATE

--- UI/PAGES/JS/Signin.js
+++ UI/PAGES/JS/Signin.js
@@ -4,7 +4,7 @@ document.getElementById('EmployeeLogin').addEventListener('submit', (e) => {
     const password = document.getElementById('password').value;
 
     if (email === 'Employee' && password === 'admin123') {
-        window.location.href = '/UI/PAGES/ADVDB.html';
+        window.location.href = '/ADV.-DB-System/UI/PAGES/ADVDB.html';
     } 
     else {
         alert('Invalid email or password. Please try again.');
